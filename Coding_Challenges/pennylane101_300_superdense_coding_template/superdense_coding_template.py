9a987582d87590cf8f7332e40bf4aaa2ce5a3792
Gonzalo Rubio <gonzrubio95@gmail.com>
2022-02-18 16:47:48 -0800

    correct

--- Coding_Challenges/pennylane101_300_superdense_coding_template/superdense_coding_template.py
+++ Coding_Challenges/pennylane101_300_superdense_coding_template/superdense_coding_template.py
@@ -9,10 +9,12 @@ dev = qml.device("default.qubit", wires=2)
 
 @qml.qnode(dev)
 def superdense_coding(bits, alpha):
-    """Construct a quantum circuit that implements superdense coding, given a not necessarily maximally entangled state
+    """Construct a quantum circuit that implements superdense coding, given a
+    not necessarily maximally entangled state
 
     Args:
-        - bits (int): 0 (binary: 00), 1 (binary: 01), 2 (binary: 10), or 3 (binary: 11), Alice's bits that she wants to communicate to Bob.
+        - bits (int): 0 (binary: 00), 1 (binary: 01), 2 (binary: 10), or 3
+        (binary: 11), Alice's bits that she wants to communicate to Bob.
         - alpha (float): angle parametrizing the entangled state
 
     Returns:
@@ -22,10 +24,25 @@ def superdense_coding(bits, alpha):
     # QHACK #
 
     # Prepare entangled state here
+    qml.Hadamard(0)
+    qml.CNOT(wires=[0, 1])
+    qml.RX(alpha - 0.25 * np.pi, wires=0)
+    qml.RY(alpha - 0.25 * np.pi, wires=1)
 
     # Implement Alice's operations on her qubit here
+    if bits == 1:
+        qml.PauliX(0)
+    elif bits == 2:
+        qml.PauliZ(0)
+    elif bits == 3:
+        qml.PauliX(0)
+        qml.PauliZ(0)
 
     # Implement Bob's measurement procedure here
+    qml.RY(alpha - 0.25 * np.pi, wires=1)
+    qml.RX(alpha - 0.25 * np.pi, wires=0)
+    qml.CNOT(wires=[0, 1])
+    qml.Hadamard(0)
 
     # QHACK #
 
@@ -33,7 +50,8 @@ def superdense_coding(bits, alpha):
 
 
 def return_probs(bits, alpha):
-    """Returns the output of the superdense_coding function for a given index (bits)"""
+    """Returns the output of the superdense_coding function for a given
+    index (bits)"""
     # DO NOT MODIFY anything in this code block
     return superdense_coding(bits, alpha)[bits].numpy()
 
@@ -41,5 +59,7 @@ def return_probs(bits, alpha):
 if __name__ == "__main__":
     # DO NOT MODIFY anything in this code block
     inputs = sys.stdin.read().split(",")
+    # inputs = [0,0.7854]
+    # inputs = [2,0.4]
     output = return_probs(int(inputs[0]), float(inputs[1]))
     print(f"{output:.6f}")
